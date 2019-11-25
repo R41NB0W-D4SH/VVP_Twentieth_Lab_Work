0c1f0f97ab103e8ce8e094fd6ef55277e61c7de8
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-25 05:24:20 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int k=0;
+	bool marker = true;
+	string s = "", s0 = "";
+	
+	cout << "Введите строку s: ";
+	getline(cin, s);
+	
+	cout << "Введите строку s0: ";
+	getline(cin, s0);
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		marker = true;
+		if (s[i] == s0[0])
+		{
+			for (int j = 1; j < s0.length(); j++)
+			{
+				if (s[i + j] != s0[j])
+				{
+					marker = false;
+				}
+			}
+			if (marker == true)
+			{
+				k++;
+				i += s0.length() - 1;
+			}
+		}
+	}
+
+	cout << "Кол-во вхождений s0 в строку s: " << k;
+	
+	system("pause");
+	return 0;
+}
