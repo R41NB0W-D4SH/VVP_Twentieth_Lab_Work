d2df2cc1b1dfb93c38ef9aa09bc67eecb615a60f
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-25 05:01:20 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	string s;
+	char c;
+	
+	cout << "Введите какую-нибудь строку: ";
+	getline(cin, s);
+
+	cout << "Введите символ C: ";
+	cin >> c;
+	
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == c)
+		{
+			cout << s[i] << s[i];
+		}
+		else 
+		{
+			cout << s[i];
+		}
+	}
+	
+	system("pause");
+	return 0;
+}
