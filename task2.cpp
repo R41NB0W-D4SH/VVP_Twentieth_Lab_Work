0d845b0415b5c7335313fb4ed67af0e0b285679c
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-25 04:40:01 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	string s;
+	int size;
+	
+	cout << "Введите строку S: ";
+	cin >> s;
+	
+	size = s.length();
+	cout << s[0];
+	
+	for (int i = 1; i < size; i++)
+	{
+		cout << " " << s[i];
+	}
+	
+	cout << endl;
+	
+	system("pause");
+	return 0;
+}
